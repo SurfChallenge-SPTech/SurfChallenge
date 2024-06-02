425afd1482d4cdd1f4194ea9b1f0056013c326eb
VictoriaGrasiele <victoria.santos@sptech.school>
2024-06-02 09:03:29 -0300

    Porcentagem da dashboard configurada com o banco

--- src/models/dashboardModel.js
+++ src/models/dashboardModel.js
@@ -3,9 +3,16 @@ var database = require("../database/config");
 function usuariosCategoria() {
     console.log("ACESSEI AS PERGUNTAS  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucaoSql = `
-    SELECT classeAtual.nome, COUNT(usuario.idUsuario) AS qtdUsuariosClasse FROM classeAtual
-LEFT JOIN usuario ON classeAtual.idClasseAtual = usuario.fkClasseAtual
-GROUP BY classeAtual.idClasseAtual, classeAtual.nome;
+    SELECT 
+    classeAtual.nome, 
+    COUNT(usuario.idUsuario) AS qtdUsuariosClasse,
+    (SELECT COUNT(*) FROM usuario) AS totalUsuarios
+FROM 
+    classeAtual
+LEFT JOIN 
+    usuario ON classeAtual.idClasseAtual = usuario.fkClasseAtual
+GROUP BY 
+    classeAtual.idClasseAtual, classeAtual.nome;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
